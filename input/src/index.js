a3debb77ff6ff2508341885908c5724e268c98d5
ehterwie <ehterwie@uwaterloo.ca>
2018-09-15 07:00:41 -0400

    Got input node app working with socket

--- input/src/index.js
+++ input/src/index.js
@@ -1,41 +1,24 @@
-const express = require("express");
-const http = require("http");
-const socket = require("socket.io");
 const record = require("node-record-lpcm16");
 const Watson = require("watson-developer-cloud/speech-to-text/v1");
-const fs = require("fs");
 const key = require("./keys/watson");
+const fs = require("fs");
+const io = require("socket.io-client");
 
-const app = express();
-const server = http.createServer(app);
-const io = socket.listen(server);
-
-const connections = [];
-
-server.listen(process.env.PORT || 3000);
-
-io.sockets.on("connection", (socket) => {
-  connections.push(socket);
-  console.log(`Connected: ${connections.length} sockets connected`);
-
-  socket.on("disconnect", () => {
-    connections.splice(connections.indexOf(socket), 1);
-    console.log(`Disconnected: ${connections.length} sockets connected`);
-  });
-});
+const socket = io("http://localhost:5000/");
 
 const watson = new Watson({
   iam_apikey: key,
   url: "https://gateway-wdc.watsonplatform.net/speech-to-text/api",
 });
 
+socket.on("newCommand", () => console.log("got new command"));
+
 const recordInterval = () => {
 
   const watsonWriteStream = fs.createWriteStream("./tempOutput.txt");
   watsonWriteStream.on("close", () => {
     fs.readFile("./tempOutput.txt", "utf8", (err, contents) => {
-      console.log("fucking sending to sockets");
-      io.sockets.emit("command", { command: contents });
+      socket.emit("sendCommand", contents );
     })
   });
 
