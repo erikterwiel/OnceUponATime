8fdf89813059b649de4a4b71952ee72e2f605d06
Danilo Joksimovic <djoksimo@uwaterloo.ca>
2018-09-15 07:05:02 -0400

    added scripts and scenes dirs

---
+++ vr/Assets/Scripts/Server.cs.cs
@@ -0,0 +1,105 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Quobject.SocketIoClientDotNet.Client;
+using Newtonsoft.Json;
+
+public class ChatData {
+	public string id;
+	public string msg;
+};
+
+public class SocketIOScript : MonoBehaviour {
+	public string serverURL = "http://localhost:3000";
+
+	public InputField uiInput = null;
+	public Button uiSend = null;
+	public Text uiChatLog = null;
+
+	protected Socket socket = null;
+	protected List<string> chatLog = new List<string> (); 
+
+	void Destroy() {
+		DoClose ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		DoOpen ();
+
+		uiSend.onClick.AddListener(() => {
+			SendChat(uiInput.text);
+			uiInput.text = "";
+			uiInput.ActivateInputField();
+		});
+	}
+
+	// Update is called once per frame
+	void Update () {
+		lock (chatLog) {
+			if (chatLog.Count > 0) {
+				string str = uiChatLog.text;
+				foreach (var s in chatLog) {
+					str = str + "\n" + s;
+				}
+				uiChatLog.text = str;
+				chatLog.Clear ();
+			}
+		}
+	}
+
+	void DoOpen() {
+		if (socket == null) {
+			socket = IO.Socket (serverURL);
+			socket.On (Socket.EVENT_CONNECT, () => {
+				lock(chatLog) {
+					// Access to Unity UI is not allowed in a background thread, so let's put into a shared variable
+					chatLog.Add("Socket.IO connected.");
+				}
+			});
+			socket.On ("chat", (data) => {
+				string str = data.ToString();
+
+				ChatData chat = JsonConvert.DeserializeObject<ChatData> (str);
+				string strChatLog = "user#" + chat.id + ": " + chat.msg;
+
+				// Access to Unity UI is not allowed in a background thread, so let's put into a shared variable
+				lock(chatLog) {
+					chatLog.Add(strChatLog);
+				}
+			});
+		}
+	}
+
+	void DoClose() {
+		if (socket != null) {
+			socket.Disconnect ();
+			socket = null;
+		}
+	}
+
+	void SendChat(string str) {
+		if (socket != null) {
+			socket.Emit ("chat", str);
+		}
+	}
+}
+
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//
+//public class Server.cs : MonoBehaviour {
+//
+//	// Use this for initialization
+//	void Start () {
+//		
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+//}
