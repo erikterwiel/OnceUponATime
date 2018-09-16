fec57b993e0d0c08ea860a47821a2cc428b020b6
ThomasGeorge <t6george@edu.uwaterloo.ca>
2018-09-15 21:31:42 -0400

    moss

--- vr/Assets/Scripts/SpawnPoint.cs
+++ vr/Assets/Scripts/SpawnPoint.cs
@@ -5,30 +5,11 @@ using System.Collections.Generic;
 public class SpawnPoint : MonoBehaviour {
 
 	public List<GameObject> storyObjects = new List<GameObject>();
+	public List<int[]> targetLocations = new List<int[]>();
 	public GameObject obj;
+	private bool toDraw = true;
 	// Use this for initialization
 	void Start () {
-		for (int i = 0; i < 5; i++) {
-			var spawnPosition = new Vector3(1f + i, 1f + i, 1f + i);
-			var spawnRotation = Quaternion.Euler(0f, 0f, 0f);
-
-			obj = new GameObject ("MUX");
-
-			if (i % 2 == 0) {
-				obj = Resources.Load ("Prefabs/FirePlace/FP2015") as GameObject;
-			} else {
-				obj = Resources.Load ("Prefabs/Wolf/kodi") as GameObject;
-			}
-
-			Debug.Log (obj.name);
-			storyObjects.Add (Instantiate(obj.gameObject, spawnPosition, spawnRotation));
-
-		}
-		//StartCoroutine (stall (5));
-
-		for (int i = 0; i < 5; i++) {
-			exitObj (i,"up");
-		}	
 			
 	}
 
@@ -38,25 +19,66 @@ public class SpawnPoint : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+
+		if (toDraw) {
+			for (int i = 0; i < 5; i++) {
+				var spawnPosition = new Vector3 (1f + i, 1f + i, 1f + i);
+				var spawnRotation = Quaternion.Euler (0f, 0f, 0f);
+
+				obj = new GameObject ("MUX");
+
+				if (i % 2 == 0) {
+					obj = Resources.Load ("Prefabs/FirePlace/FP2015") as GameObject;
+				} else {
+					obj = Resources.Load ("Prefabs/Wolf/kodi") as GameObject;
+				}
+
+				Debug.Log (obj.name);
+				storyObjects.Add (Instantiate (obj.gameObject, spawnPosition, spawnRotation));
+
+			}
+
+			toDraw = false;
+	 
+
+		}
+
+		for (int i = 0; i < 5; i++) {
+			movObj (i, "back", 10);
+
+		}
+			
+
 	}
 
-	void deleteObj (int index) {
+	void destroyObj (int index) {
 		Destroy(storyObjects[index].gameObject);
 		storyObjects.RemoveAt (index);
 	}
 
-	void exitObj (int index, string direction) {
+	void movObj (int index, string direction, int magnitude) {
 
-		Rigidbody rb = storyObjects[index].AddComponent<Rigidbody>();
-		
 		switch (direction)
 		{
 		case "up":
-			//Vector3 moss = new Vector3 (10,10,10);
-			rb.AddForce(transform.forward);
+			storyObjects [index].transform.Translate(Vector3.up * Time.deltaTime * magnitude);
 			break;
-
+		case "down":
+			storyObjects [index].transform.Translate(-Vector3.up * Time.deltaTime * magnitude);
+			break;
+		case "left":
+			storyObjects [index].transform.Translate(-Vector3.right * Time.deltaTime * magnitude);
+			break;
+		case "right":
+			storyObjects [index].transform.Translate(Vector3.right * Time.deltaTime * magnitude);
+			break;
+		case "back":
+			storyObjects [index].transform.Translate(-Vector3.forward * Time.deltaTime * magnitude);
+			break;
+		case "forward":
+			storyObjects [index].transform.Translate(Vector3.forward * Time.deltaTime * magnitude);
+			break;
 
 		default:
 			break;
